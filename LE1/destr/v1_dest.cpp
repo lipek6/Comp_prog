10add8a5023ea095eeed167a7eb310f36b506301
fald <felipeaugustolira2006@gmail.com>
2025-09-22 21:01:55 -0300

    Finnished dandelions destruction challenge

--- LE1/destr/v1_dest.cpp
+++ LE1/destr/v1_dest.cpp
@@ -10,7 +10,7 @@ int main (void)
     long long t;             // Amount of test cases
     long long n;             // Amount of fields
     long long entry;         // Input of how many dandelions in one field
-    long long accumulator;   // Amount of dandelions harvesteds (we put everything in here, after that we remove what shouldn't be here)
+    long long accumulator;   // Amount of dandelions harvesteds
 
     std::cin >> t;              // Gets the amount of test cases
     
@@ -19,17 +19,21 @@ int main (void)
     {
         std::cin >> n;                                 // Gets the amount of fields
 
+        std::vector<long long> evens;                  // Evens inside inputs
         std::vector<long long> odds;                   // Odds inside inputs
         std::vector<long long>::iterator it;
         accumulator = 0;
 
-        // Gets amount of dandelions per field and sorts it in odds, also put everything in the accumulator
+        // Gets amount of dandelions per field and sorts it in evens an odds
         for (i = 0; i < n; i++)                     
         {
             std::cin >> entry;
-            accumulator += entry;
 
-            if(entry % 2 != 0)
+            if(entry % 2 == 0)
+            {
+                evens.push_back(entry);
+            }
+            else
             {
                 odds.push_back(entry);
             }
@@ -38,17 +42,27 @@ int main (void)
         // If he nevers can turn on his lawnmower
         if(odds.empty())
         {
-            std::cout << 0 << "\n";
+            std::cout << accumulator << "\n";
             continue;
         }
 
         std::sort(odds.begin(), odds.end());
         
-        long long middle = std::ceil(odds.size()/2);
-        
-        for(i = 0; i < middle; i++)
+        for (i = 0; i < evens.size(); i++)
         {
-            accumulator -= odds[i];
+            accumulator += evens[i];
+        }
+        
+        if (odds.size() > 2)
+        {
+            for (it = odds.end(); it != (odds.begin() + odds.size()/2) - 1; --it)
+            {
+                accumulator += *it;
+            }
+        }
+        else
+        {
+            accumulator += odds.back();
         }
 
         std::cout << accumulator << "\n";
