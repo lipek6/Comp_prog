e6b9b400bc53195061b732b095bcb666529084fa
fald <felipeaugustolira2006@gmail.com>
2025-10-14 11:25:40 -0300

    Working on the TLE of EX4 - a

--- EXAMPLES/EX4/a/a.cpp
+++ EXAMPLES/EX4/a/a.cpp
@@ -1,33 +1,15 @@
 #include<bits/stdc++.h>
 
-int fact(int n)
-{
-    int fact_n = 1;
-    for(int i = 1; i < n+1; i++)
-    {
-        fact_n = fact_n * i;
-    }  
-    return fact_n;
-}
-
-std::vector<int> available_people(int n)
-{
-    std::vector<int> teens(n);
-    for (int i = 0; i < n; i++)
-    {
-        teens[i] = i;
-    }
-    return teens;
-}
 
 
 int main (void)
 {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(nullptr);
-    
-    int a, b, c;
     int n, m;           // Size of the group and amount of constraints
+    int a, b, c;
+    
+    int valid;
     while(true)
     {
         std::cin >> n >> m;
@@ -35,42 +17,62 @@ int main (void)
         {
             break;
         }
-
+        
+        int fact_n = 1;
+        for(int i = 1; i < n + 1; i++)
+        {
+            fact_n = fact_n * i;
+        }
+    
+        std::vector<std::vector<int>> all (fact_n, std::vector<int>(n, 0));        
         std::vector<int> group(n);
         for(int i = 0; i < n; i++)
         {
             group[i] = i;
         }
 
-        std::vector<std::vector<int>(n)> tree(n);
-        int depth = 0;
-        for(int i = 0; i < n; i++)
+        for(int i = 0; i < fact_n; i++)
         {
-            tree[i][depth] = group[i];
-            depth++;
-            for(int j = 0; j < n - depth; j++)
-            {
-                tree[i][j + depth] = group[j + depth];
-                depth++;
-                for(int k = 0; k < n - depth; k++)
-                {
-                    tree[i][k + depth] = group[k + depth];
-                }
-
-            }
+            all[i] = group;
+            std::next_permutation(group.begin(), group.end());
         }
-
-        while(m--)
+        
+        valid = fact_n;    
+        std::vector<int> invalid;
+        for(int i = 0; i < m; i++)
         {
             std::cin >> a >> b >> c;
-            
+            for(int j = 0; j < all.size(); j++)
+            {
+                int pos_a = -1;
+                int pos_b = -1;
+                for(int k = 0; k < n; k++)
+                {
+                    if(pos_a >= 0 && pos_b >= 0) break;
+                    if (all[j][k] == a) pos_a = k;
+                    if (all[j][k] == b) pos_b = k;
+                }
+                
+                int dist = std::abs(pos_a - pos_b);
+                
+                if(c >= 0)
+                {
+                    if(dist > abs(c) && !std::count(invalid.begin(), invalid.end(), j))
+                    {
+                        invalid.push_back(j);
+                        valid--;
+                    }
+                }
+                else
+                {
+                    if(dist < abs(c) && !std::count(invalid.begin(), invalid.end(), j))
+                    {
+                        invalid.push_back(j);
+                        valid--;
+                    }
+                }
+            }
         }
+        std::cout << valid << "\n";
     }
-
-
-
-
-
-
-
 }
