6c67e281e0ec77d9107dbddd1cec000c2e4c7db8
fald <felipeaugustolira2006@gmail.com>
2025-10-21 12:00:38 -0300

    Added Competitive Programming books to the repository

--- LISTS_OF_EXERCISES/LE5/b/digit.cpp
+++ LISTS_OF_EXERCISES/LE5/b/digit.cpp
@@ -5,20 +5,23 @@ int main(void){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    string n; cin >> n;
+    int n; cin >> n;
 
-    vector<int> d(n.size());
-
-    int n_int = -1;
-    while (n_int != 0)
+    int steps = 0;
+    while (n != 0)
     {
-        for(int i = 0; i < n.size(); i++)
-        {
-            d[i] = n[i] - '0';
-            n_int += d[i] * pow(10, n.size()-1);
-        }
-        sort(d.begin(), d.end(), greater());
+        int max_digit = 0;
         
-        n = n - d[0];
+        int temp = n;
+        while(temp != 0)        // Finds the greatest digit of n
+        {
+            int last_digit = temp % 10;             // Gets the last digit of n
+            max_digit = max(max_digit, last_digit); // Finds the maximum between all the digits of n (max starts at 0)
+            temp = temp / 10;                       // Removes the last digit of n, so we can look ar the other digits at last_digit and compare than to find the greatest
+        }
+        n -= max_digit;         // Removes the greatest digit of n from n
+        steps++;                // Counts the steps
     }
+
+    cout << steps << "\n";
 }
