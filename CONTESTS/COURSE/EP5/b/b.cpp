bb9443ed62dee7a1395a540e0ed03ab07aacaa67
fald <felipeaugustolira2006@gmail.com>
2025-10-22 09:29:34 -0300

    Successfully upsolved question b of the EP5. It was a dp, the point is to keep control of the BEST total cost to get at each rock and use it as reference we calculating the next best total cost, that way we can avoid solving for the best local solution instead of the best global solution.

--- CONTESTS/COURSE/EP5/b/b.cpp
+++ CONTESTS/COURSE/EP5/b/b.cpp
@@ -14,8 +14,17 @@ int main(void)
     vector<int> h(n + 1);
     for(int i = 1; i < n + 1; i++) cin >> h[i];
 
-    int dp[i][c][]
+    vector<long long> dp(n + 1, LLONG_MAX);     // total optimal costs at each rock
+    dp[1] = 0;                                  // Base case, from rock 1 to rock 1 the cost is h1 - h1 = 0
 
+    for (int i = 2; i < n + 1; i++)            
+    {
+        for (int j = i - 1; j >= 1 && j >= i - k; j--)      // Check where we can go from j to i (backwards), considering the k constraint and the bottom of the vector
+        {
+            long long cost = dp[j] + abs(h[i] - h[j]);      // Total cost considering the current path, it's the cost + the old costs
+            dp[i] = min(dp[i], cost);                       // Select the best option to put into the dp
+        }
 
-
+    }
+    cout << dp[n] << endl;                                  // dp[n] contains the optimal minimal cost to get from 1 to n
 }
