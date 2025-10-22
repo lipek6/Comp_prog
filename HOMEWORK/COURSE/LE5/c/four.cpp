84694ba3e09745cf9fa98fdc4041ea8f635ec0fc
fald <felipeaugustolira2006@gmail.com>
2025-10-22 14:05:51 -0300

    Heelp with dp

--- HOMEWORK/COURSE/LE5/c/four.cpp
+++ HOMEWORK/COURSE/LE5/c/four.cpp
@@ -1,33 +1,46 @@
 #include<bits/stdc++.h>
 using namespace std;
+using ll = long long;
 
 int main (void)
 {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    // We want the minimum cost. It can be greedy or dp, but it is probably dp
-    
-    // Using a greedy algorithm, we can look at the cost per person of each car and put the students
-    // on the cars from the lowest to the biggest cost per person
     
     int x, n; cin >> x >> n;           // Number of marathonists and available ride options
 
-    vector<int> p(n);       // Passengers
-    vector<int> v(n);       // Values
+    vector<ll> p(n);       // Passengers
+    vector<ll>  v(n);       // Values
+    
+    ll total_capacity = 0;
     for(int i = 0; i < n; i++)
     {
         cin >> p[i] >> v[i];        // Number of passengers and the charge for the trip
+        total_capacity += p[i];
     }
 
-    vector<int>dp(n)
-    for(int i = 0; i < n; i++)
+    if (x > total_capacity)
     {
-        for(int j = 0; j < n; j++)
-        {
-            dp[i] = v[j]
-
-        }   
+        cout << "-1" << "\n";
+        return 0;        
     }
+    
+    vector<pair<ll, ll>> dp(n);    // Remaining people and total value dp
+    dp[0].first  = x - p[0];
+    dp[0].second = 0 + v[0];
 
+    for(int i = 1; i < n; i++)
+    {
+        for(int j = i - 1; j >= 0; j--)
+        {
+            ll ratio  = dp[j].second / dp[j].first;
+            ll remain = x - dp[j].first;
+            ll value  = dp[j] + dp[i];
 
+            if(ratio < dp[i] / )
+            // ith and jth needs to go to zero
+        }
+    }
+    
+    cout << dp[n-1].second << endl;
 }
