197e99522afc2590d5b390e37f3527028a302752
fald <felipeaugustolira2006@gmail.com>
2025-10-23 12:02:27 -0300

    BFS

---
+++ TEMPLATES/GRAPHS/BFS/bfs.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main (void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    vector<int> dist(v, INFINITY)   // The distance vector stores the distance between the source node and the node, but also stores the info if it was or was not visited before
+
+
+
+}
