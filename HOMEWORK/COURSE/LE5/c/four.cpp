88692e2266e90303222d5a0f277cb0d5161b011a
fald <felipeaugustolira2006@gmail.com>
2025-10-24 15:06:08 -0300

    ewf

--- HOMEWORK/COURSE/LE5/c/four.cpp
+++ HOMEWORK/COURSE/LE5/c/four.cpp
@@ -4,13 +4,21 @@ using ll = long long;
 
 int main (void)
 {
+    // The (fucking) idea here is to make dp hold the optimal cost to get to the total capacity of passangers:
+    // So: dp[0] = 0; 
+    //     dp[1] = best price to alocate 1 guy  at the cars;
+    //     dp[2] = best price to alocate 2 guys at the cars;
+    //     dp[3] = best price to alocate 3 guys at the cars;
+    //     dp[4] = best price to alocate 4 guys at the cars...
+
+
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     
     int x, n; cin >> x >> n;           // Number of marathonists and available ride options
 
     vector<ll> p(n);       // Passengers
-    vector<ll>  v(n);       // Values
+    vector<ll> v(n);       // Values
     
     ll total_capacity = 0;
     for(int i = 0; i < n; i++)
@@ -25,22 +33,29 @@ int main (void)
         return 0;        
     }
     
-    vector<pair<ll, ll>> dp(n);    // Remaining people and total value dp
-    dp[0].first  = x - p[0];
-    dp[0].second = 0 + v[0];
+    vector<ll> dp(total_capacity + 1, LLONG_MAX);    // Minimal cost to get to a total capacity of idx
+    dp[0] = 0;
 
-    for(int i = 1; i < n; i++)
+    for(int i = 0; i < n; i++)
     {
-        for(int j = i - 1; j >= 0; j--)
+        int capacity = p[i];
+        int cost     = v[i];
+        for(int k = total_capacity; k >= capacity; k--)
         {
-            ll ratio  = dp[j].second / dp[j].first;
-            ll remain = x - dp[j].first;
-            ll value  = dp[j] + dp[i];
-
-            if(ratio < dp[i] / )
-            // ith and jth needs to go to zero
+            if (dp[k - capacity] != LLONG_MAX)
+            {
+                dp[k] = min(dp[k], dp[k - capacity] + cost);
+            }
         }
     }
-    
-    cout << dp[n-1].second << endl;
+
+    // cout << dp[x] << "\n"; We cannot simply print the cost to transport x guys
+    // There is the chance that trasporting more than x guys is cheaper
+
+    ll min_cost = LLONG_MAX;
+    for (int k = x; k <= total_capacity; k++)
+    {
+        min_cost = min(min_cost, dp[k]);
+    }
+    cout << min_cost << "\n";
 }
