3e08125647bbfa1bc499c1df3f6c93acfe577e58
fald <felipeaugustolira2006@gmail.com>
2025-10-30 14:53:49 -0300

    Finnished 'j' question of the HOMEWORK of the SELECTIVE, about STL and prefix_sum/sufix_sum

--- HOMEWORK/SELECTIVE/STL/j/j.cpp
+++ HOMEWORK/SELECTIVE/STL/j/j.cpp
@@ -1,9 +1,43 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// The neighbors of each number is the most important thing
+// Prefixal / Sufixal min max?
 int main (void)
 {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    int t; cin >> t;
+
+    while(t--)
+    {
+        int n; cin >> n;
+        vector<int> a(n);
+        vector<int> maxs(n);
+        vector<int> mins(n);
+        
+        int max_val = 0;
+        int min_val = INT_MAX;
+
+        for(int i = 0; i < n; i++) cin >> a[i];
+        for(int i = 0; i < n; i++) 
+        {
+            max_val = max(max_val, a[n - i - 1]);
+            min_val = min(min_val, a[i]);
+            mins[i] = min_val; 
+            maxs[n - i - 1] = max_val;      // backwards
+        }
+        
+        cout << '1';            // The borders are always ok (I think) 
+        for(int i = 1; i < n - 1; i++)
+        {  
+            if(a[i] > mins[i] && a[i] < maxs[i])
+                cout << '0';
+            else
+                cout << '1';
+            cout.flush();
+        }
+        cout << '1' << "\n";    // The borders are always ok (I think)
+    }
 }
--- HOMEWORK/SELECTIVE/STL/m/m.cpp
+++ HOMEWORK/SELECTIVE/STL/m/m.cpp
@@ -1,9 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// We need to find the median of windows of size 'k', that menas that we will sort the k elements and get the middle element
+// OK, I will use multiseeeet
+
 int main (void)
 {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    int n, k; cin >> n >> k;
+    vector<int> x(n);
+    multiset<int> window;    // Value and index
+    
+    for(int i = 0; i < n; i++) 
+    {
+        cin >> x[i];
+    }
+    
 }
