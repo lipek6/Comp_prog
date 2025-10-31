4d76b878cc04d1eec3f6ea32e2d40f94c55af309
fald <felipeaugustolira2006@gmail.com>
2025-10-30 21:28:26 -0300

    Merging

--- HOMEWORK/SELECTIVE/STL/e/e.cpp
+++ HOMEWORK/SELECTIVE/STL/e/e.cpp
@@ -6,4 +6,34 @@ int main (void)
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    string t, u; cin >> t >> u;
+
+    int tsize = t.length();
+    int usize = u.length();
+
+    int substring_size = 0;
+    int j = 0;
+    for(int i = 0; i < tsize; i++)
+    {
+        
+        while((t[i] == u[j] || t[i] == '?') && (i < tsize && j < usize))                  // If 't' is matching to the substring 
+        {
+            substring_size++;
+            i++;
+            j++;
+        }
+        if(substring_size == usize)                         // If we found the substring
+        {
+            cout << "Yes" << "\n";
+            return 0;
+        }       
+        else if(substring_size != 0)                        // if not found (and a check to see if we even got into the while loop)    
+        {
+            i -= substring_size;
+            substring_size = 0;
+            j = 0;
+        }
+    }
+
+    cout << "No" << "\n";
 }
