e88fd2da6404232177063421eed3a5fb0b219222
fald <felipeaugustolira2006@gmail.com>
2025-10-31 12:01:12 -0300

    Working on the i/movies question

--- HOMEWORK/SELECTIVE/STL/i/i.cpp
+++ HOMEWORK/SELECTIVE/STL/i/i.cpp
@@ -1,9 +1,38 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// I NEED TO CHECK ALLLLL THE FUCKING POSSIBLE PAIRS OF HIM GETTING TO THE MOVIES AND GETTING OUT OF THE MOVIES
 int main (void)
 {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
+
+    int tc; cin >> tc;
+
+    while(tc--)
+    {
+        int n, m; cin >> n >> m;    // Amount of movies and moments
+        vector<int> l(n);           // Start of the movie
+        vector<int> r(n);           // End of the movie
+        for(int i = 0; i < n; i++) cin >> l[i] >> r[i]  // Getting movies windows of exhibition
+
+        for(int i = 0; i < m; i++)  // Moments analises
+        {
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
 
 }
