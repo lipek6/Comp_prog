f8551bc5dc275d1badcc346934c8064e45702438
fald <felipeaugustolira2006@gmail.com>
2025-11-11 19:13:33 -0300

    Trees are cool

--- HOMEWORK/SELECTIVE/GRAPHS_1/e/e.cpp
+++ HOMEWORK/SELECTIVE/GRAPHS_1/e/e.cpp
@@ -6,4 +6,6 @@ int main (void)
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    
+
 }
--- HOMEWORK/SELECTIVE/GRAPHS_1/g/g.cpp
+++ HOMEWORK/SELECTIVE/GRAPHS_1/g/g.cpp
@@ -1,8 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define UNVISITED -1
 
 vector<vector<int>> AL;
+vector<int> dist;
 
+// If we want the maximum distance between two nodes, we just need to run a BFS :)
 int main (void)
 {
     ios_base::sync_with_stdio(false);
@@ -10,7 +13,8 @@ int main (void)
 
     int n, m; cin >> n >> m;
     
-    AL.assign(n+1);
+    AL.resize(n+1);
+    
     for(int i = 0; i < m; i++)
     {
         int guy1, guy2;
@@ -19,21 +23,46 @@ int main (void)
         AL[guy1].push_back(guy2);
         AL[guy2].push_back(guy1);
     }
-
-    int minimum_bonds = m;
     
-    for(int i = 1; i <= n; i++)
+    
+    int k = 0;
+    for(int i = 1; i <= n; i++) // Run a BFS on each node to find the maximum distance
     {
-        if(dfs_states[i] == UNVISITED)
-        {
-            dfs(i);
+        dist.assign(n+1, UNVISITED);
+        queue<int> q; 
+        q.push(i);
+        dist[i] = 0;
 
+        while(!q.empty())
+        {
+            int source_node = q.front();
+            q.pop();
+
+            for(int adjacent_node : AL[source_node])
+            {
+                if(dist[adjacent_node] == UNVISITED)
+                {
+                    dist[adjacent_node] = dist[source_node] + 1;
+                    q.push(adjacent_node);
+                    k = max(k, dist[adjacent_node]);
+                }
+            }
+        }
+
+        // Check if it is impossible to connect two people
+        if(i == 1)
+        {
+            for(int j = 1; j <= n; j++)
+                {
+                    if(dist[j] == UNVISITED)
+                    {
+                        cout << "=[" << "\n";
+                        return 0;
+                    }
+                }
         }
     }
 
-
-
-
-
+    cout << "=] " << k << "\n";
 
 }
