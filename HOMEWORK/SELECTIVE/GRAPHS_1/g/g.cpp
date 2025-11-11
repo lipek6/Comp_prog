5c554a8bc967e5ef4b502f16994062db2ffe0e13
fald <felipeaugustolira2006@gmail.com>
2025-11-11 14:38:04 -0300

    I wanna go home

--- HOMEWORK/SELECTIVE/GRAPHS_1/g/g.cpp
+++ HOMEWORK/SELECTIVE/GRAPHS_1/g/g.cpp
@@ -1,9 +1,39 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+vector<vector<int>> AL;
+
 int main (void)
 {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
+
+    int n, m; cin >> n >> m;
+    
+    AL.assign(n+1);
+    for(int i = 0; i < m; i++)
+    {
+        int guy1, guy2;
+        cin >> guy1 >> guy2;
+        
+        AL[guy1].push_back(guy2);
+        AL[guy2].push_back(guy1);
+    }
+
+    int minimum_bonds = m;
+    
+    for(int i = 1; i <= n; i++)
+    {
+        if(dfs_states[i] == UNVISITED)
+        {
+            dfs(i);
+
+        }
+    }
+
+
+
+
+
 
 }
