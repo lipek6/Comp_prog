9c9d46a275d446aba7d240797bbd783c00031a1c
fald <felipeaugustolira2006@gmail.com>
2025-11-14 19:32:10 -0300

    Third place hihi

--- HOMEWORK/SELECTIVE/GRAPHS_1/e/e.cpp
+++ HOMEWORK/SELECTIVE/GRAPHS_1/e/e.cpp
@@ -1,11 +1,50 @@
 #include<bits/stdc++.h>
+#define INF 1e18
 using namespace std;
+
 
 int main (void)
 {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-
     
+    int n; cin >> n;
 
+    vector<vector<pair<int, long long>>> AL(n+1);
+    for(int i = 1; i <= n; i++)
+    {
+        int a, b;
+        long long w;
+        cin >> a >> b >> w;    
+        AL[a].push_back({b, w});
+        AL[b].push_back({a, w});
+    }
+
+    vector<long long> dist(n+1, INF);
+    dist[1] = 0;
+
+    queue<int> q;
+    q.push(1);
+
+    while(!q.empty())
+    {
+        int current_node = q.front();
+        q.pop();
+
+        for(auto& [adjacent_node, realative_distance] : AL[current_node])
+        {
+            if(dist[adjacent_node] != INF) continue;
+
+            dist[adjacent_node] = dist[current_node] + realative_distance;
+            q.push(adjacent_node);
+        }
+    }
+
+    for(int i = 1; i <= n; i++)
+    {
+        if(dist[i] % 2 == 0)
+            cout << "0" << "\n";
+        else
+            cout << "1" << "\n";
+    }
 }
--- HOMEWORK/SELECTIVE/GRAPHS_1/i/i.cpp
+++ HOMEWORK/SELECTIVE/GRAPHS_1/i/i.cpp
@@ -1,9 +1,48 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+enum {NO_ROAD = 0, CIRCLE_ROAD = 1, NEW_ROAD = 2};
+
+// Yeah, that probably requires a matrix and not an adjacency list
 int main (void)
 {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
+
+    int n, m; cin >> n >> m;
+    vector<vector<int>> matrix(n+1, vector<int>(n+1, NO_ROAD));
+
+    for(int i = 1; i <= n; i++)
+    {
+        for(int j = 1; j <= n; j++)
+        {
+            if(j == i+1)
+            matrix[i][j] = CIRCLE_ROAD;
+            else if(i == j+1)
+            matrix[i][j] = CIRCLE_ROAD;
+        }
+    }
+    matrix[n][1] = CIRCLE_ROAD;
+    matrix[1][n] = CIRCLE_ROAD;
+
+    string result = "";
+
+    for(int i = 0; i < m; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+
+        if(matrix[a][b] == CIRCLE_ROAD)
+        {
+            result.append("i");
+        }
+        else if(matrix[a][b] == NO_ROAD)
+        {
+            result.append("o");
+        }
+    }
+
+    cout << result << "\n";
+
 
 }
