4c8efb9bceee78f333bb511b48ee8a54dbab3846
fald <felipeaugustolira2006@gmail.com>
2025-11-18 10:16:59 -0300

    aaa ,help eme

--- HOMEWORK/SELECTIVE/GRAPHS_1/f/f.cpp
+++ HOMEWORK/SELECTIVE/GRAPHS_1/f/f.cpp
@@ -1,9 +1,44 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int dir_row[40] = {-1, +1, +0, +0}; // Up, Down, Left, Right
+int dir_col[40] = {+0, +0, -1, +1}; // Up, Down, Left, Right
+
+int n, m; 
+vector<vector<char>> matrix;
+
+struct coordinate
+{
+    int x;
+    int y;
+    coordinate (int x_position, int y_position) : x(x_position), y(y_position) {} 
+};
+
+
+// Each character is . (floor), # (wall), A (start), or M (monster). There is exactly one A in the input.
 int main (void)
 {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
+
+    cin >> n >> m;
+    matrix.resize(n, vector<char>(m));
+
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < m; j++)
+        {
+            char point; cin >> point;
+            matrix[i][j] = point;
+
+            if(point == 'A')
+            {
+                A.x = j;
+                A.y = i;
+            }
+        }
+    }
+
+
 
 }
