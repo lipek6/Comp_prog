e1e3b38f22b15a1292c44f235acbf0c33f568ce1
fald <felipeaugustolira2006@gmail.com>
2025-11-28 12:26:02 -0300

    GOing home 2 2

--- HOMEWORK/SELECTIVE/GRAPHS_2/l/l.cpp
+++ HOMEWORK/SELECTIVE/GRAPHS_2/l/l.cpp
@@ -2,72 +2,30 @@
 #define INF 1e18
 using namespace std;
 
-vector<vector<pair<long long,int>>> AL;
+vector<vector<long long>> parent;
+vector<vector<long long>> AM;
 
-void dijkstra(int source, int destiny, vector<long long>& dist, vector<long long>& parent);
+long long full, L;
+int n, m, queries;
+
+int get_path(long long source, long long destiny);
+void floyd_warshall();
+void get_input();
 
 int main (void)
 {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int n, m; long long L; cin >> n >> m >> L;
-    long long full = L;
+    get_input();
+    floyd_warshall();
 
-    AL.resize(n + 1);
-
-    for(int i = 0; i < m; i++)
-    {
-        int a, b; long long c; cin >> a >> b >> c;
-
-        AL[a].push_back({c, b});
-        AL[b].push_back({c, a});
-    }
-
-    int queries; cin >> queries;
-    
-    
     while(queries--)
     {
-        vector<long long> dist(n + 1, INF);
-        vector<long long> parent(n + 1, INF);
-
         int source, destiny; cin >> source >> destiny;
         
-        dijkstra(source, destiny, dist, parent);
-
-        vector<int> path;
-        int node = destiny;
-        while(parent[node] != node)
-        {
-            path.push_back(node);
-            node = parent[node];
-        } 
-        path.push_back(source);
-        reverse(path.begin(), path.end());
-
-        long long recharges = 0;
-        bool impossible = false;
-        for(int i = 1; i < path.size(); i++)
-        {
-            long long spent_litters = dist[path[i]] - dist[path[i-1]];
-
-            if(L - spent_litters < 0)
-            {
-                L = full;
-                recharges++;
-            }
-
-            L -= spent_litters;
-
-            if(L < 0)
-            {
-                impossible = true;
-                break;    
-            }
-        }
-
-        if(impossible)
+        int recharges = get_path(source, destiny);
+        if(recharges < 0)
         {
             cout << "-1\n";
         }
@@ -78,31 +36,87 @@ int main (void)
     }
 }
 
-void dijkstra(int source, int destiny, vector<long long>& dist, vector<long long>& parent)
+void get_input()
 {
-    priority_queue<pair<long long, long long>> heap;
-    parent[source] = source;
-    heap.push({0, source});
-    dist[source] = 0;
+    cin >> n >> m >> L;
+    full = L;
 
-    while(!heap.empty())
+    parent.resize(n + 1, vector<long long>(n + 1, INF));
+    AM.resize(n + 1, vector<long long>(n + 1, INF));
+
+    for(int i = 1; i <= m; i++)
     {
-        auto [curr_legth, curr_node] = heap.top();
-        heap.pop();
+        int a, b; long long c; cin >> a >> b >> c;
 
-        if(curr_legth > dist[curr_node]) continue;          // Trash on the heap
-        
-        for(auto [adj_length, adj_node] : AL[curr_node])
+        AM[a][b] = c;
+        AM[b][a] = c;
+    }
+
+    for(int i = 1; i <= n; i++) AM[i][i] = 0;
+
+    cin >> queries;
+}
+
+void floyd_warshall()
+{
+    for(int i = 1; i <= n; i++)
+    {
+        for(int j = 1; j <= n; j++)
         {
-            long long old_length = dist[adj_node];
-            long long new_length = dist[curr_node] + adj_length;
+            parent[i][j] = i;
+        }
+    }
 
-            if(new_length < old_length)
+    for(int k = 1; k <= n; k++)
+    {
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = 1; j <= n; j++)
             {
-                dist[adj_node] = new_length;
-                parent[adj_node] = curr_node;
-                heap.push({new_length, adj_node});
+                long long detour_distance = AM[i][k] + AM[k][j];
+                long long direct_distance = AM[i][j];
+                if(detour_distance < direct_distance)
+                {
+                    AM[i][j] = detour_distance;
+                    parent[i][j] = parent[k][j];
+                }
             }
         }
     }
+}
+
+int get_path(long long source, long long destiny)
+{
+    if(AM[source][destiny] == INF) return -1;
+    
+    L = full;
+    int recharges = 0;
+    
+    vector<pair<int,int>> path;
+    while(source != destiny)
+    {
+        path.push_back({destiny, parent[source][destiny]});
+        destiny = parent[source][destiny];
+    }
+    path.push_back({source, destiny});
+    reverse(path.begin(), path.end());
+
+    for(int i = 0; i < path.size(); i++)
+    {
+        long long spent_litters = AM[path[i].first] - AM[path[i].second];
+
+        if(L - spent_litters < 0)
+        {
+            L = full;
+            recharges++;
+        }
+
+        L -= spent_litters;
+
+        if(L < 0)
+        {
+            return -1;
+        }
+    }
+    return recharges;
 }
