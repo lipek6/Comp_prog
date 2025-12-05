41e25cf3aba6b8bdd196ef0b4cb75bb6a92ceb86
fald <felipeaugustolira2006@gmail.com>
2025-12-05 10:22:02 -0300

    done

---
+++ HOMEWORK/SELECTIVE/DP_1/a/a.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main (void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, k; cin >> n >> k;
+
+    vector<int> h(n);
+    for(int i = 0; i < n; i++) cin >> h[i];
+
+    vector<vector<int>> memo(n, vector<int>(n));
+
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            
+        }
+    }
+
+
+
+
+
+
+
+
+}
